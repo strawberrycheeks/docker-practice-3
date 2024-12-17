f754bc773c0b8e3d1997232169cca61f5a9626c7
Ekaterina <abramushkinakatya@gmail.com>
2024-12-18 02:18:14 +0300

    Добавит скрипт

--- docker-cleanup.sh
+++ docker-cleanup.sh
@@ -1,1 +1,19 @@
-# TODO: написать скрипт, используя команды из списка
+#!/bin/bash
+
+# Удаление всех остановленных контейнеров
+docker container prune -f
+
+# Удаление всех неиспользуемых образов
+docker image prune -a -f
+
+# Удаление незакрепленных томов
+docker volume prune -f
+
+# Удаление неиспользуемых сетей
+docker network prune -f
+
+# Удаление старого кэша сборки
+docker builder prune -f
+
+# Дополнительная очистка логов контейнеров
+truncate -s 0 /var/lib/docker/containers/*/*-json.log
